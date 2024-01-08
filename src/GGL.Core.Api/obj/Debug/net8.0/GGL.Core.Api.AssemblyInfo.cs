5a87a6069db7491bea6e42ba3695bb02d3dc50eb
masjad86 <m.asjad1986@gmail.com>
2024-01-08 11:28:16 +0530

    Commit.

--- src/GGL.Core.Api/obj/Debug/net8.0/GGL.Core.Api.AssemblyInfo.cs
+++ src/GGL.Core.Api/obj/Debug/net8.0/GGL.Core.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GGL.Core.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da27abaa658f26d1e1a598f7b44bac929009401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GGL.Core.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GGL.Core.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
